3d2e556ab3f0365e5995766aa43bda58337d277b
Ella Herlihy <ellaherlihy@gmail.com>
2023-10-06 14:52:43 +0100

    First commit

---
+++ src/components/FindOpenPubs.js
@@ -0,0 +1,60 @@
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
+
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+
+export default function FindOpenPubs() {
+  const [latitude, setLatitude] = useState(null);
+  const [longitude, setLongitude] = useState(null);
+  const [error, setError] = useState(null);
+  const [bars, setBars] = useState([]);
+
+  useEffect(() => {
+    if ("geolocation" in navigator) {
+      navigator.geolocation.getCurrentPosition(
+        (position) => {
+          setLatitude(position.coords.latitude);
+          setLongitude(position.coords.longitude);
+        },
+        (err) => {
+          setError(err.message);
+        }
+      );
+    } else {
+      setError("Geolocation is not available in your browser.");
+    }
+  }, []);
+
+  useEffect(() => {
+    const fetchData = async () => {
+        const response = await axios.get(
+          'https://maps.googleapis.com/maps/api/place/nearbysearch/json',
+          {
+            params: {
+              open_now: true,
+              location: `${latitude},${longitude}`,
+              radius: 500, // Adjust the radius as needed (in meters)
+              keyword: 'pub',
+              key: apiKey,
+              minrating: 4,
+            },
+          }
+        );
+        setBars(response.data.results.slice(0, 5));
+        // console.log(response.data.results[4].vicinity);
+      };
+    fetchData();
+    },[latitude, longitude]);
+
+  return (
+    <div className="font-mono">
+      <h2 className="text-3xl font-bold underline">Nearest Pubs</h2>
+      {error && <p>{error}</p>}
+      <ul className="list-none">
+        {bars.map((bar, index) => (
+          <li key={index}>{bar.name}</li>
+        ))}
+      </ul>
+    </div>
+  );
+}
---
+++ src/components/OpeningHours.js
@@ -0,0 +1,57 @@
+import React, { useState } from 'react';
+import axios from 'axios';
+// require('dotenv').config()
+
+const apiKey = process.env.REACT_APP_GOOGLE_API_KEY;
+
+function OpeningHours() {
+  const [locationName, setLocationName] = useState('');
+  // const [openingHours, setOpeningHours] = useState(null);
+
+  const searchLocation = () => {
+    axios
+      .get(`https://maps.googleapis.com/maps/api/place/textsearch/json?query=${locationName}&key=${apiKey}`)
+      .then((response) => {
+        console.log(response.data.results[0].opening_hours.open_now)
+        // if (response.data.results && response.data.results.length > 0) {
+        //   const placeId = response.data.results[0].place_id;
+        //   fetchOpeningHours(placeId);
+        // } else {
+        //   setOpeningHours(null);
+        // }
+      })
+      .catch((error) => {
+        console.error('Error searching for location:', error);
+      });
+  };
+
+  // const fetchOpeningHours = (placeId) => {
+  //   axios
+  //     .get(`https://maps.googleapis.com/maps/api/place/details/json?place_id=${placeId}&fields=opening_hours&key=${apiKey}`)
+  //     .then((response) => {
+  //       if (response.data.result && response.data.result.opening_hours) {
+  //         setOpeningHours(response.data.result.opening_hours);
+  //       } else {
+  //         setOpeningHours(null);
+  //       }
+  //     })
+  //     .catch((error) => {
+  //       console.error('Error fetching opening hours:', error);
+  //     });
+  // };
+
+  return (
+    <div>
+      <h2>Where can I get a drink!!</h2>
+      <input
+        type="text"
+        placeholder="Enter location name"
+        value={locationName}
+        onChange={(e) => setLocationName(e.target.value)}
+      />
+      <button onClick={searchLocation}>Search</button>
+    </div>
+  );
+}
+
+export default OpeningHours;
